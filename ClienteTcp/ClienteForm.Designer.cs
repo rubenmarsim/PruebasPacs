8515c648d4c3271e9159175757e859e97d5fcc1a
Mike <mj.egea@sarria.salesians.cat>
2018-12-18 17:36:08 +0100

    client message

--- ClienteTcp/ClienteForm.Designer.cs
+++ ClienteTcp/ClienteForm.Designer.cs
@@ -28,21 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sendBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // sendBox
+            // 
+            this.sendBox.Location = new System.Drawing.Point(206, 69);
+            this.sendBox.Name = "sendBox";
+            this.sendBox.Size = new System.Drawing.Size(100, 20);
+            this.sendBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Send";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(669, 377);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connectar";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
             // 
             // ClienteForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendBox);
             this.Name = "ClienteForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ClienteForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox sendBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConnect;
     }
 }
 
--- ClienteTcp/ClienteForm.cs
+++ ClienteTcp/ClienteForm.cs
@@ -22,10 +22,9 @@ namespace ClienteTcp
 
         private void ClienteForm_Load(object sender, EventArgs e)
         {
-            Connect("127.0.0.1");
         }
 
-        void Connect(String server)
+        void Connect(String server,string message)
         {
 
             try
@@ -37,16 +36,59 @@ namespace ClienteTcp
 
                 Int32 port = 5000;
                 TcpClient client = new TcpClient(server, port);
-                
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+
+
+
+
                 client.Close();
             }catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
             }
 
+
+
            
         }
 
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (sendBox.Text != string.Empty)
+            {
+                Connect("127.0.0.1");
+            }
+            else {
+                MessageBox.Show("tiendes que escribir algo en el textbox");
+            }
+           
+        }
     }
 }
 
